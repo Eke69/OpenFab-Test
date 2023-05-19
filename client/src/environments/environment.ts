1f7953508fbdf515d8289e7675fcdaa52a6646fe
Eke Kelechi <e.kelechi93@gmail.com>
2023-05-19 09:06:36 +0100

    fixed another error with url naming

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'openfab-test-api.onremote.com', // Example API URL
+    apiUrl: 'https://openfab-test-api.onremote.com', // Example API URL
     // Add more environment variables as needed
   };
