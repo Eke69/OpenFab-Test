66a979156b35f8afb0615db0114dacf8d5577ff1
Eke Kelechi <e.kelechi93@gmail.com>
2023-05-19 02:32:59 +0100

    fixed error with url naming

--- client/src/app/constants/urls.ts
+++ client/src/app/constants/urls.ts
@@ -1,6 +1,7 @@
 import { environment } from "src/environments/environment";
+import { environment as _env} from "src/environments/environment.prod";
 
-export const BASE_URL = environment.apiUrl;
+export const BASE_URL = _env.production ? _env.apiUrl : environment.apiUrl;
 export const SIGNUP_URL = BASE_URL + '/users/signup';
 export const LOGIN_URL = BASE_URL + '/users/login';
 export const PRODUCTS_URL = BASE_URL + '/products/';
