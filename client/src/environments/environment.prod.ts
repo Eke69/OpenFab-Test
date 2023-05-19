4aa7182bf1630090c04d98956bf06e7eff44526f
Eke Kelechi <e.kelechi93@gmail.com>
2023-05-19 09:59:30 +0100

    fixed another error with url naming v3

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://openfab-test-api.onremote.com', // Example API URL
+    apiUrl: 'https://openfab-test-api.onrender.com', // Example API URL
     // Add more environment variables as needed
   };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://openfab-test-api.onremote.com', // Example API URL
+    apiUrl: 'https://openfab-test-api.onrender.com', // Example API URL
     // Add more environment variables as needed
   };
